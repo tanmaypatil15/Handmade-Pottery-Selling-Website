50d53d616bcbcd22a1a98d4010d899a8d0b8d705
Tanmay Patil <patiltanmay033@gmail.com>
2022-02-12 09:10:50 +0530

    update settings.py

--- Handmade/settings.py
+++ Handmade/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["handmade-pottery.herokuapp.com", "localhost"]
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'store'
     
 
